ef44efde5fe063a8eda73ac47eeb0e5ea67d9f07
riccardovecchi0101 <riccardo.vecchi0101@gmail.com>
2025-03-25 10:08:00 +0100

    firstChanges

--- mainApp.py
+++ mainApp.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask("SmartBin")
 
 @app.route("/") #app route si riferisce direttamente alla funzione
 def home():
-    return "<html><h1> Hello world </h1></html>"
+    return render_template('index.html')
+
+@app.route("/index/")
+def index():
+    return home()
 
 @app.route("/prova")
 def prova():
