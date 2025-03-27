8c5b79c1517fa9371783ccf589836d046a448866
riccardovecchi0101 <riccardo.vecchi0101@gmail.com>
2025-03-27 16:06:41 +0100

    dbInitialized

--- mainApp.py
+++ mainApp.py
@@ -1,7 +1,19 @@
 from flask import Flask
 from flask import render_template
+from flask_migrate import Migrate
+from models import db, Utente
+
+import secrets
+
+secret_key = secrets.token_hex(16) #good for develop, bad for production (ma stica)
+app.config['SECRET_KEY'] = secret_key
 
 app = Flask("SmartBin")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db) 
 
 @app.route("/") #app route si riferisce direttamente alla funzione
 def home():
@@ -14,3 +26,9 @@ def index():
 @app.route("/prova")
 def prova():
     return "<html><b> Pagina di prova <b></html>"
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(debug=True)
