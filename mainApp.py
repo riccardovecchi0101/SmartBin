705d7774c59b03242b8f1aec64071bf198831ca3
giuseppebellissimo <253999@studenti.unimore.it>
2025-03-28 13:59:29 +0100

    Create "First Access" function

--- mainApp.py
+++ mainApp.py
@@ -1,31 +1,49 @@
-from flask import Flask
-from flask import render_template
-from flask_migrate import Migrate
-from models import db, Utente
-
 import secrets
 
-secret_key = secrets.token_hex(16) #good for develop, bad for production (ma stica)
-app.config['SECRET_KEY'] = secret_key
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_migrate import Migrate
+
+from models import *
+
+secret_key = secrets.token_hex(16)  # good for develop, bad for production (ma stica)
 
 app = Flask("SmartBin")
+app.config['SECRET_KEY'] = secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-migrate = Migrate(app, db) 
+migrate = Migrate(app, db)
 
-@app.route("/") #app route si riferisce direttamente alla funzione
+
+@app.route("/")  # app route si riferisce direttamente alla funzione
 def home():
     return render_template('index.html')
 
-@app.route("/index/")
+
+@app.route("/index")
 def index():
     return home()
 
-@app.route("/prova")
-def prova():
-    return "<html><b> Pagina di prova <b></html>"
+
+@app.route("/firstAccess", methods=['GET', 'POST'])
+def firstAccess():
+    if request.method == 'POST':
+        uuid_input = request.form.get('uuid')
+        inserviente = Inserviente.query.filter_by(uuid=uuid_input).first()
+
+        if not inserviente:
+            flash("UUID non valido!", "danger")
+            return redirect(url_for('firstAccess'))
+
+        if inserviente.username:
+            flash("UUID già registrato. Effettua il login.", "warning")
+            return redirect(url_for('login'))
+
+        return redirect(url_for('signup'))
+
+    return render_template('firstAccess.html')
+
 
 with app.app_context():
     db.create_all()
--- models.py
+++ models.py
@@ -2,12 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Utente(db.Model):
+
+class Inserviente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100))
-    cognome = db.Column(db.String(100))
-    UUID = db.Column(db.String(36))
-    password = db.Column(db.String(25))
+    uuid = db.Column(db.String(36), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=True)
+    surname = db.Column(db.String(100), nullable=True)
+    username = db.Column(db.String(100), unique=True, nullable=True)
+    piano = db.Column(db.String(10), nullable=True)
+    password = db.Column(db.String(100), nullable=True)
 
     def __repr__(self):
-        return f"<Utente {self.nome}, >"+ ":"+ " "+ "UUID:" +self.UUID
+        return f"<Inserviente {self.name}, >" + ":" + " " + "UUID:" + self.uuid
